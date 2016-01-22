b2065a2a311304060fa5b5eb34e59585001abd6a
Mike Goren <mgoren@mortalwombat.net>
2016-01-21 22:40:31 -0800

    add address object with constructor and fullAddress prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,16 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
