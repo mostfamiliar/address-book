cdc722161ff300640c08e82d28292fe9dff25dca
Mike Goren <mgoren@mortalwombat.net>
2016-01-21 22:57:23 -0800

    refactor out resetFields code

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,14 @@ Address.prototype.fullAddress = function() {
   return this.street + ", " + this.city + ", " + this.state;
 }
 
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+}
+
 $(document).ready(function() {
 
   $("#add-address").click(function() {
@@ -65,11 +73,7 @@ $(document).ready(function() {
       });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
 
   });
 });
