e82165558407a5cfaa33a5202c447f5eb7f6909b
alissa paluska and torrence stratton <alissa.paluska@gmail.com>
2016-01-25 11:56:11 -0800

    Business logic functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,47 @@
-var toDoList = function(item){
-  return item;
+function toDoList(category) {
+  this.category = category;
+  this.items = [];
 };
+
+function toDoItem(string) {
+  this.string = string;
+};
+
+toDoList.prototype.combinedInfo = function() {
+  var output = this.category + ": ";
+
+  for (var i=0; i < this.items.length; i++){
+        var item = this.items[i];
+        console.log(item.string);
+        output += item.string + " ";
+  };
+  output = output.slice(0, output.length - 1);  // remove space at end
+
+  return output;
+};
+
+
+
+$(document).ready(function() {
+  $("form#toDoList").submit(function(event) {
+    event.preventDefault();
+    var inputCategory = $(this).find("input#category").val();
+    var inputToDoItem = $(this).find("input#toDoItem").val();
+
+    var listItem = new Item(inputToDoItem);
+    var combinedList = new toDoList(inputCategory);
+
+    combinedList.items.push(listItem);
+
+    console.log(combinedList);
+    $("ul.itemPrintOut").append("<li>" + combinedList.combinedInfo() + "</li>");
+
+    combinedList.items.forEach(function(listObject){
+
+    });
+
+    // $("ul#itemCompleted").append(combinedList);
+    console.log(combinedList);
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,19 @@
 describe("toDoList", function() {
- it("will allow the user to enter a task", function() {
-   expect(toDoList("New Task")).to.equal("New Task");
+ it("creates a new list with the given information", function() {
+   var newTask = new toDoList("House");
+   expect(newTask.category).to.equal("House");
+   expect(newTask.items).to.eql([]);
  });
+ it("it will return a formatted string with category and item", function() {
+     var newList = new toDoList("House");
+     var newItem = new toDoItem("Dishes");
+     newList.items.push(newItem);
+     expect(newList.combinedInfo()).to.equal("House: Dishes");
+  });
+});
+describe('toDoItem', function() {
+  it("create a new item", function() {
+    var testToDoItem = new toDoItem("Dishes");
+    expect(testToDoItem.string).to.equal("Dishes");
+  });
 });
